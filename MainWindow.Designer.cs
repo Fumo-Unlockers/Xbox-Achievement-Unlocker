1a38466cd74a01f81a044de269998f91b4494c38
Draff <xboxjack1@gmail.com>
2022-04-09 05:32:20 +0100

    Can now grab recent games Populates a panel with game names + images Images are clickable and grabs list of achivements

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -37,6 +37,8 @@ namespace Xbox_Achievement_Unlocker
             this.BTN_XUID = new System.Windows.Forms.Button();
             this.LBL_Gamertag = new System.Windows.Forms.Label();
             this.LBL_Gamerscore = new System.Windows.Forms.Label();
+            this.Panel_Recents = new System.Windows.Forms.Panel();
+            this.BTN_Recents = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // BGWorker
@@ -85,9 +87,9 @@ namespace Xbox_Achievement_Unlocker
             // 
             // BTN_XUID
             // 
-            this.BTN_XUID.Location = new System.Drawing.Point(417, 56);
+            this.BTN_XUID.Location = new System.Drawing.Point(666, 5);
             this.BTN_XUID.Name = "BTN_XUID";
-            this.BTN_XUID.Size = new System.Drawing.Size(107, 23);
+            this.BTN_XUID.Size = new System.Drawing.Size(122, 23);
             this.BTN_XUID.TabIndex = 5;
             this.BTN_XUID.Text = "Grab Info";
             this.BTN_XUID.UseVisualStyleBackColor = true;
@@ -111,11 +113,30 @@ namespace Xbox_Achievement_Unlocker
             this.LBL_Gamerscore.TabIndex = 7;
             this.LBL_Gamerscore.Text = "Gamerscore: ";
             // 
+            // Panel_Recents
+            // 
+            this.Panel_Recents.Location = new System.Drawing.Point(12, 102);
+            this.Panel_Recents.Name = "Panel_Recents";
+            this.Panel_Recents.Size = new System.Drawing.Size(776, 336);
+            this.Panel_Recents.TabIndex = 8;
+            // 
+            // BTN_Recents
+            // 
+            this.BTN_Recents.Location = new System.Drawing.Point(666, 56);
+            this.BTN_Recents.Name = "BTN_Recents";
+            this.BTN_Recents.Size = new System.Drawing.Size(122, 23);
+            this.BTN_Recents.TabIndex = 9;
+            this.BTN_Recents.Text = "Grab Recent Games";
+            this.BTN_Recents.UseVisualStyleBackColor = true;
+            this.BTN_Recents.Click += new System.EventHandler(this.BTN_Recents_Click);
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BTN_Recents);
+            this.Controls.Add(this.Panel_Recents);
             this.Controls.Add(this.LBL_Gamerscore);
             this.Controls.Add(this.LBL_Gamertag);
             this.Controls.Add(this.BTN_XUID);
@@ -141,6 +162,8 @@ namespace Xbox_Achievement_Unlocker
         private System.Windows.Forms.Button BTN_XUID;
         private System.Windows.Forms.Label LBL_Gamertag;
         private System.Windows.Forms.Label LBL_Gamerscore;
+        private System.Windows.Forms.Panel Panel_Recents;
+        private System.Windows.Forms.Button BTN_Recents;
     }
 }
 
