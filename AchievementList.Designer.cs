6a78c436d286bf56003940036c04d2bf8152af09
Draff <xboxjack1@gmail.com>
2022-04-10 05:48:20 +0100

    Added second form Second form used to parse achievement list Can currently display unlock status, name and description Currently using testdata as to not spam servers with api requests during debugging

---
+++ AchievementList.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Xbox_Achievement_Unlocker
+{
+    partial class AchievementList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            AchievementList.Panel_AchievementList = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // Panel_AchievementList
+            // 
+            AchievementList.Panel_AchievementList.Location = new System.Drawing.Point(0, 0);
+            AchievementList.Panel_AchievementList.Name = "Panel_AchievementList";
+            AchievementList.Panel_AchievementList.Size = new System.Drawing.Size(775, 450);
+            AchievementList.Panel_AchievementList.TabIndex = 0;
+            // 
+            // AchievementList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(AchievementList.Panel_AchievementList);
+            this.Name = "AchievementList";
+            this.Text = "AchievementList";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public static System.Windows.Forms.Panel Panel_AchievementList;
+    }
+}
---
+++ AchievementList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+using System.Threading;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace Xbox_Achievement_Unlocker
+{
+    public partial class AchievementList : Form
+    {
+        public AchievementList()
+        {
+            InitializeComponent();
+        }
+
+        public static async void PopulateAchievementList(string AchievementData)
+        {
+            Form form = new AchievementList();
+            form.Show();
+            var Jsonresponse = (dynamic)(new JObject());
+            Jsonresponse = (dynamic)JObject.Parse(AchievementData);
+            var count = 0;
+            var newline = 0;
+            for (int i = 0; i < Jsonresponse.achievements.Count; i++)
+            {
+                if (Panel_AchievementList.Height < newline+55)
+                    Panel_AchievementList.Height = Panel_AchievementList.Height + 35;
+                RichTextBox AchievementName = new RichTextBox();
+                AchievementName.Location = new System.Drawing.Point(12, 12 + newline);
+                AchievementName.Size = new System.Drawing.Size(150, 50);
+                AchievementName.Name = "name_"+Jsonresponse.achievements[i].id.ToString();
+                AchievementName.Text = Jsonresponse.achievements[i].name.ToString();
+                Panel_AchievementList.Controls.Add(AchievementName);
+                RichTextBox AchievementUnlocked = new RichTextBox();
+                AchievementUnlocked.Location = new System.Drawing.Point(167, 12 + newline);
+                AchievementUnlocked.Size = new System.Drawing.Size(80, 50);
+                AchievementUnlocked.Name = "Unlocked_"+Jsonresponse.achievements[i].id.ToString();
+                AchievementUnlocked.Text = Jsonresponse.achievements[i].progressState.ToString();
+                Panel_AchievementList.Controls.Add(AchievementUnlocked);
+                RichTextBox AchievementDescription = new RichTextBox();
+                AchievementDescription.Location = new System.Drawing.Point(252, 12 + newline);
+                AchievementDescription.Size = new System.Drawing.Size(300, 50);
+                AchievementDescription.Name = "Description_" + Jsonresponse.achievements[i].id.ToString();
+                AchievementDescription.Text = Jsonresponse.achievements[i].description.ToString();
+                Panel_AchievementList.Controls.Add(AchievementDescription);
+                newline = newline + 55;
+            }
+
+
+        }
+    }
+}
