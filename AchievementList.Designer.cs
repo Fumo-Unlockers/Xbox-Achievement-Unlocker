50337fc553af3917b565c0b796d5ba69fac5bfd6
Draff <xboxjack1@gmail.com>
2022-04-14 13:13:56 +0100

    UI works final list read needs fixing

--- AchievementList.Designer.cs
+++ AchievementList.Designer.cs
@@ -29,31 +29,129 @@ namespace Xbox_Achievement_Unlocker
         /// </summary>
         private void InitializeComponent()
         {
-            AchievementList.Panel_AchievementList = new System.Windows.Forms.Panel();
+            this.Panel_AchievementList = new System.Windows.Forms.Panel();
+            this.Header3 = new System.Windows.Forms.TextBox();
+            this.Header2 = new System.Windows.Forms.TextBox();
+            this.Header1 = new System.Windows.Forms.TextBox();
+            this.Header4 = new System.Windows.Forms.TextBox();
+            this.BTN_Unlock = new System.Windows.Forms.Button();
+            this.CheckBox_Images = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // Panel_AchievementList
             // 
-            AchievementList.Panel_AchievementList.Location = new System.Drawing.Point(0, 0);
-            AchievementList.Panel_AchievementList.Name = "Panel_AchievementList";
-            AchievementList.Panel_AchievementList.Size = new System.Drawing.Size(775, 450);
-            AchievementList.Panel_AchievementList.TabIndex = 0;
+            this.Panel_AchievementList.AutoScroll = true;
+            this.Panel_AchievementList.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Panel_AchievementList.Location = new System.Drawing.Point(0, 0);
+            this.Panel_AchievementList.Margin = new System.Windows.Forms.Padding(0);
+            this.Panel_AchievementList.Name = "Panel_AchievementList";
+            this.Panel_AchievementList.Size = new System.Drawing.Size(800, 405);
+            this.Panel_AchievementList.TabIndex = 0;
+            // 
+            // Header3
+            // 
+            this.Header3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Header3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Header3.Location = new System.Drawing.Point(198, 0);
+            this.Header3.Margin = new System.Windows.Forms.Padding(0);
+            this.Header3.Name = "Header3";
+            this.Header3.ReadOnly = true;
+            this.Header3.Size = new System.Drawing.Size(293, 23);
+            this.Header3.TabIndex = 5;
+            this.Header3.Text = "Description";
+            this.Header3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Header2
+            // 
+            this.Header2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Header2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Header2.Location = new System.Drawing.Point(47, 0);
+            this.Header2.Margin = new System.Windows.Forms.Padding(0);
+            this.Header2.Name = "Header2";
+            this.Header2.ReadOnly = true;
+            this.Header2.Size = new System.Drawing.Size(151, 23);
+            this.Header2.TabIndex = 3;
+            this.Header2.Text = "Name";
+            this.Header2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Header1
+            // 
+            this.Header1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Header1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Header1.Location = new System.Drawing.Point(0, 0);
+            this.Header1.Margin = new System.Windows.Forms.Padding(0);
+            this.Header1.Name = "Header1";
+            this.Header1.ReadOnly = true;
+            this.Header1.Size = new System.Drawing.Size(47, 23);
+            this.Header1.TabIndex = 2;
+            this.Header1.Text = "Unlock";
+            this.Header1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Header4
+            // 
+            this.Header4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Header4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Header4.Location = new System.Drawing.Point(491, 0);
+            this.Header4.Margin = new System.Windows.Forms.Padding(0);
+            this.Header4.Name = "Header4";
+            this.Header4.ReadOnly = true;
+            this.Header4.Size = new System.Drawing.Size(292, 23);
+            this.Header4.TabIndex = 4;
+            this.Header4.Text = "Stats";
+            this.Header4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BTN_Unlock
+            // 
+            this.BTN_Unlock.Location = new System.Drawing.Point(0, 405);
+            this.BTN_Unlock.Margin = new System.Windows.Forms.Padding(0);
+            this.BTN_Unlock.Name = "BTN_Unlock";
+            this.BTN_Unlock.Size = new System.Drawing.Size(105, 46);
+            this.BTN_Unlock.TabIndex = 6;
+            this.BTN_Unlock.Text = "Unlock Achievements";
+            this.BTN_Unlock.UseVisualStyleBackColor = true;
+            this.BTN_Unlock.Click += new System.EventHandler(this.BTN_Unlock_Click);
+            // 
+            // CheckBox_Images
+            // 
+            this.CheckBox_Images.AutoSize = true;
+            this.CheckBox_Images.Enabled = false;
+            this.CheckBox_Images.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.CheckBox_Images.Location = new System.Drawing.Point(435, 416);
+            this.CheckBox_Images.Name = "CheckBox_Images";
+            this.CheckBox_Images.Size = new System.Drawing.Size(365, 24);
+            this.CheckBox_Images.TabIndex = 7;
+            this.CheckBox_Images.Text = "Show images instead of stats (disabled until I fix it)";
+            this.CheckBox_Images.UseVisualStyleBackColor = true;
+            this.CheckBox_Images.CheckedChanged += new System.EventHandler(this.CheckBox_Images_CheckedChanged);
             // 
             // AchievementList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(AchievementList.Panel_AchievementList);
+            this.Controls.Add(this.Header1);
+            this.Controls.Add(this.CheckBox_Images);
+            this.Controls.Add(this.BTN_Unlock);
+            this.Controls.Add(this.Header3);
+            this.Controls.Add(this.Header4);
+            this.Controls.Add(this.Header2);
+            this.Controls.Add(this.Panel_AchievementList);
             this.Name = "AchievementList";
             this.Text = "AchievementList";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        public static System.Windows.Forms.Panel Panel_AchievementList;
+        private System.Windows.Forms.Panel Panel_AchievementList;
+        private System.Windows.Forms.TextBox Header1;
+        private System.Windows.Forms.TextBox Header2;
+        private System.Windows.Forms.TextBox Header4;
+        private System.Windows.Forms.TextBox Header3;
+        private System.Windows.Forms.Button BTN_Unlock;
+        private System.Windows.Forms.CheckBox CheckBox_Images;
     }
 }
--- AchievementList.cs
+++ AchievementList.cs
@@ -21,41 +21,193 @@ namespace Xbox_Achievement_Unlocker
         {
             InitializeComponent();
         }
+        public List<string> AchievementIDs = new List<string>();
 
-        public static async void PopulateAchievementList(string AchievementData)
+        public async void PopulateAchievementList(string AchievementData)
         {
-            Form form = new AchievementList();
-            form.Show();
+            AchievementList ALForm= new AchievementList();
+            ALForm.Show();
             var Jsonresponse = (dynamic)(new JObject());
             Jsonresponse = (dynamic)JObject.Parse(AchievementData);
-            var count = 0;
             var newline = 0;
+            var backcolour = Color.Silver;
             for (int i = 0; i < Jsonresponse.achievements.Count; i++)
             {
-                if (Panel_AchievementList.Height < newline+55)
-                    Panel_AchievementList.Height = Panel_AchievementList.Height + 35;
-                RichTextBox AchievementName = new RichTextBox();
-                AchievementName.Location = new System.Drawing.Point(12, 12 + newline);
-                AchievementName.Size = new System.Drawing.Size(150, 50);
-                AchievementName.Name = "name_"+Jsonresponse.achievements[i].id.ToString();
+                //hacky thing because im kinda retarded and dont want to do proper ui design
+                Panel PanelLine = new Panel();
+                PanelLine.Height = 144;
+                PanelLine.Width = 782;
+                PanelLine.BackColor = Color.Transparent;
+                PanelLine.BorderStyle = BorderStyle.FixedSingle;
+                PanelLine.Location = new System.Drawing.Point(0, 23+newline);
+                PanelLine.Name = "Line_" + i.ToString();
+                ALForm.Panel_AchievementList.Controls.Add(PanelLine);
+                var PanelLineElement = ALForm.Panel_AchievementList.Controls.OfType<Panel>().Last();
+                //background for unlocked section
+                Panel AchievementUnlockedBack = new Panel();
+                AchievementUnlockedBack.Enabled = false;
+                AchievementUnlockedBack.BorderStyle = BorderStyle.None;
+                AchievementUnlockedBack.BackColor = backcolour;
+                AchievementUnlockedBack.Location = new System.Drawing.Point(0, 0);
+                AchievementUnlockedBack.Size = new System.Drawing.Size(45, 144);
+                AchievementUnlockedBack.Name = "UnlockedBack_" + Jsonresponse.achievements[i].id.ToString();
+                PanelLineElement.Controls.Add(AchievementUnlockedBack);
+                //foreground for unlock section
+                CheckBox AchievementUnlocked = new CheckBox();
+                AchievementUnlocked.Location = new System.Drawing.Point(15, 64);
+                AchievementUnlocked.Text = " ";
+                AchievementUnlocked.Name = Jsonresponse.achievements[i].id.ToString();
+                AchievementUnlocked.Width = 15;
+                AchievementUnlocked.Height = 14;
+                AchievementUnlocked.BackColor = backcolour;
+                AchievementUnlocked.Click += new System.EventHandler(this.SelectAchievement);
+                PanelLineElement.Controls.Add(AchievementUnlocked);
+                AchievementUnlocked.BringToFront();
+                //background for name section
+                Panel AchievementNameBack = new Panel();
+                AchievementNameBack.Enabled = false;
+                AchievementNameBack.BackColor = backcolour;
+                AchievementNameBack.BorderStyle = BorderStyle.None;
+                AchievementNameBack.Location = new System.Drawing.Point(47, 0);
+                AchievementNameBack.Size = new System.Drawing.Size(149, 144);
+                AchievementNameBack.Name = "NameBack_"+Jsonresponse.achievements[i].id.ToString();
+                PanelLineElement.Controls.Add(AchievementNameBack);
+                //foreground for name section
+                Label AchievementName = new Label();
+                AchievementName.BackColor = backcolour;
+                AchievementName.Location = new System.Drawing.Point(47, 0);
+                AchievementName.MinimumSize = new System.Drawing.Size(149, 144);
+                AchievementName.MaximumSize = new System.Drawing.Size(149, 144);
+                AchievementName.Name = "Name_" + Jsonresponse.achievements[i].id.ToString();
                 AchievementName.Text = Jsonresponse.achievements[i].name.ToString();
-                Panel_AchievementList.Controls.Add(AchievementName);
-                RichTextBox AchievementUnlocked = new RichTextBox();
-                AchievementUnlocked.Location = new System.Drawing.Point(167, 12 + newline);
-                AchievementUnlocked.Size = new System.Drawing.Size(80, 50);
-                AchievementUnlocked.Name = "Unlocked_"+Jsonresponse.achievements[i].id.ToString();
-                AchievementUnlocked.Text = Jsonresponse.achievements[i].progressState.ToString();
-                Panel_AchievementList.Controls.Add(AchievementUnlocked);
-                RichTextBox AchievementDescription = new RichTextBox();
-                AchievementDescription.Location = new System.Drawing.Point(252, 12 + newline);
-                AchievementDescription.Size = new System.Drawing.Size(300, 50);
+                PanelLineElement.Controls.Add(AchievementName);
+                AchievementName.BringToFront();
+                //background for description section
+                Panel AchievementDescriptionBack = new Panel();
+                AchievementDescriptionBack.Enabled = false;
+                AchievementDescriptionBack.BackColor = backcolour;
+                AchievementDescriptionBack.BorderStyle = BorderStyle.None;
+                AchievementDescriptionBack.Location = new System.Drawing.Point(198, 0);
+                AchievementDescriptionBack.Size = new System.Drawing.Size(291, 144);
+                AchievementDescriptionBack.Name = "DescriptionBack_" + Jsonresponse.achievements[i].id.ToString();
+                PanelLineElement.Controls.Add(AchievementDescriptionBack);
+                //foreground for description section
+                Label AchievementDescription = new Label();
+                AchievementDescription.BackColor = backcolour;
+                AchievementDescription.Location = new System.Drawing.Point(198, 0);
+                AchievementDescription.MinimumSize = new System.Drawing.Size(291, 144);
+                AchievementDescription.MaximumSize = new System.Drawing.Size(291, 144);
                 AchievementDescription.Name = "Description_" + Jsonresponse.achievements[i].id.ToString();
                 AchievementDescription.Text = Jsonresponse.achievements[i].description.ToString();
-                Panel_AchievementList.Controls.Add(AchievementDescription);
-                newline = newline + 55;
+                PanelLineElement.Controls.Add(AchievementDescription);
+                AchievementDescription.BringToFront();
+                //background for stats/image section
+                Panel StatsBack = new Panel();
+                StatsBack.Enabled = false;
+                StatsBack.BackColor = backcolour;
+                StatsBack.BorderStyle = BorderStyle.None;
+                StatsBack.Location = new System.Drawing.Point(491, 0);
+                StatsBack.Size = new System.Drawing.Size(292, 144);
+                StatsBack.Name = "StatsBack_" + Jsonresponse.achievements[i].id.ToString();
+                PanelLineElement.Controls.Add(StatsBack);
+                //foreground for stats section
+                Label Stats = new Label();
+                Stats.BackColor = backcolour;
+                Stats.BorderStyle = BorderStyle.None;
+                Stats.Location = new System.Drawing.Point(491, 0);
+                Stats.MinimumSize = new System.Drawing.Size(292, 144);
+                Stats.MaximumSize = new System.Drawing.Size(292, 144);
+                Stats.Name = "Stats_" + Jsonresponse.achievements[i].id.ToString();
+                Stats.Text = "Gamerscore: " + Jsonresponse.achievements[i].rewards[0].value.ToString() +
+                             "\nRarity: " + Jsonresponse.achievements[i].rarity.currentCategory.ToString() +
+                             "\nPlayer Percentage: " + Jsonresponse.achievements[i].rarity.currentPercentage.ToString() + "%" +
+                             "\nSecret: " + Jsonresponse.achievements[i].isSecret.ToString() +
+                             "\nProgress State: " + Jsonresponse.achievements[i].progressState.ToString();
+                PanelLineElement.Controls.Add(Stats);
+                Stats.BringToFront();
+                /*foreground for images section
+                PictureBox AchievementImage = new PictureBox();
+                AchievementImage.BackColor = backcolour;
+                AchievementImage.BorderStyle = BorderStyle.None;
+                AchievementImage.Location = new System.Drawing.Point(491, 0);
+                AchievementImage.Size = new System.Drawing.Size(292, 144);
+                AchievementImage.Name = "Image_" + Jsonresponse.achievements[i].id.ToString();
+                AchievementImage.LoadAsync(Jsonresponse.achievements[i].mediaAssets[0].url.ToString()+ "&w=280&h=280&format=jpg");
+                AchievementImage.Visible = false;*/
+                //change some ui elements if achievement is already unlocked
+                if (Jsonresponse.achievements[i].progressState.ToString()== "Achieved")
+                {
+                    Stats.Text = "Gamerscore: " + Jsonresponse.achievements[i].rewards[0].value.ToString() +
+                                 "\nRarity: " + Jsonresponse.achievements[i].rarity.currentCategory.ToString() +
+                                 "\nPlayer Percentage: " + Jsonresponse.achievements[i].rarity.currentPercentage.ToString() + "%" +
+                                 "\nSecret: " + Jsonresponse.achievements[i].isSecret.ToString() +
+                                 "\nProgress State: " + Jsonresponse.achievements[i].progressState.ToString() +
+                                 "\nUnlock Time: " + Jsonresponse.achievements[i].progression.timeUnlocked.ToString();
+                    AchievementUnlocked.Enabled = false;
+                    AchievementUnlocked.Checked = true;
+                }
+                newline = newline + 144;
+                backcolour = Color.Silver;
+                if (i % 2 == 0)
+                {
+                    backcolour = Color.FromArgb(224, 224, 224);
+                }
             }
+        }
+        void SelectAchievement(object sender,EventArgs e)
+        {
+            CheckBox SelectedAchievement = (sender as CheckBox);
+            if (SelectedAchievement.Checked)
+            {
+                AchievementIDs.Add(SelectedAchievement.Name);
+                foreach (string id in AchievementIDs)
+                {
+                    MessageBox.Show(id);
+                }
 
+            }
+            else
+            {
+                AchievementIDs.Remove(SelectedAchievement.Name.ToString());
+                foreach (string id in AchievementIDs)
+                {
+                    MessageBox.Show(id);
+                }
 
+            }
+        }
+        private void CheckBox_Images_CheckedChanged(object sender, EventArgs e)
+        {
+            //man idk this doesnt work and I dont really care that much about images. leaving it for now
+            AchievementList ALForm = new AchievementList();
+            if (CheckBox_Images.Checked)
+            {
+                ALForm.Header4.Text = "Images";
+                foreach (PictureBox box in ALForm.Panel_AchievementList.Controls.OfType<PictureBox>())
+                {
+                    box.Visible = true;
+                    box.BringToFront();
+                }
+            }
+            else
+            {
+                ALForm.Header4.Text = "Stats";
+                foreach (PictureBox box in ALForm.Panel_AchievementList.Controls.OfType<PictureBox>())
+                {
+                    box.Visible = false;
+                    box.SendToBack();
+                }
+            }
+            
+        }
+
+        void BTN_Unlock_Click(object sender, EventArgs e)
+        {
+            foreach (string id in AchievementIDs)
+            {
+                MessageBox.Show(id);
+            }
+            
         }
     }
 }
