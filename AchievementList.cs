d37c9f4720360264d620a865656150d307ba87ba
ItsLogic <comamnds@outlook.com>
2022-09-10 23:03:19 +0100

    Improved the achivements list so its now less shit filtered out xbox 360 titles added error checking for games with 0 achivements bumped to dotnet 6

--- AchievementList.cs
+++ AchievementList.cs
@@ -28,122 +28,64 @@ namespace Xbox_Achievement_Unlocker
         List<string> UnlockableAchievements = new List<string>();
         public async void PopulateAchievementList(string AchievementData)
         {
-
-            AchievementList ALForm = new AchievementList();
-            ALForm.Show();
             var Jsonresponse = (dynamic)(new JObject());
             Jsonresponse = (dynamic)JObject.Parse(AchievementData);
             var newline = 0;
             var backcolour = Color.Silver;
-            ALForm.SCID = Jsonresponse.achievements[0].serviceConfigId.ToString();
-            ;
-            ALForm.TitleID = Jsonresponse.achievements[0].titleAssociations[0].id.ToString();
-            for (int i = 0; i < Jsonresponse.achievements.Count; i++)
+            try
             {
-                if (Jsonresponse.achievements[0].progression.requirements.ToString().Length > 2)
+                SCID = Jsonresponse.achievements[0].serviceConfigId.ToString();
+                TitleID = Jsonresponse.achievements[0].titleAssociations[0].id.ToString();
+                for (int i = 0; i < Jsonresponse.achievements.Count; i++)
                 {
-                    if (Jsonresponse.achievements[0].progression.requirements[0].id != "00000000-0000-0000-0000-000000000000")
-                        MessageBox.Show("This game might use event based achivements and if so currently does not work", "Warning");
-                    break;
+                    if (Jsonresponse.achievements[0].progression.requirements.ToString().Length > 2)
+                    {
+                        if (Jsonresponse.achievements[0].progression.requirements[0].id != "00000000-0000-0000-0000-000000000000")
+                            MessageBox.Show("This game might use event based achivements and if so currently does not work", "Warning");
+                        break;
+                    }
                 }
+
+                for (int i = 0; i < Jsonresponse.achievements.Count; i++)
+                {
+                    if (Jsonresponse.achievements[i].progressState.ToString() == "Achieved")
+                    {
+                        DGV_AchievementList.Rows.Add(2,
+                            Jsonresponse.achievements[i].name.ToString(),
+                            Jsonresponse.achievements[i].description.ToString(),
+                            "Gamerscore: " + Jsonresponse.achievements[i].rewards[0].value.ToString() +
+                            "\nRarity: " + Jsonresponse.achievements[i].rarity.currentCategory.ToString() +
+                            "\nPlayer Percentage: " + Jsonresponse.achievements[i].rarity.currentPercentage.ToString() + "%" +
+                            "\nSecret: " + Jsonresponse.achievements[i].isSecret.ToString() +
+                            "\nProgress State: " + Jsonresponse.achievements[i].progressState.ToString() +
+                            "\nUnlock Time: " + Jsonresponse.achievements[i].progression.timeUnlocked.ToString(),
+                            Jsonresponse.achievements[i].id.ToString()
+                        );
+                    }
+                    else
+                    {
+                        DGV_AchievementList.Rows.Add(0,
+                            Jsonresponse.achievements[i].name.ToString(),
+                            Jsonresponse.achievements[i].description.ToString(),
+                            "Gamerscore: " + Jsonresponse.achievements[i].rewards[0].value.ToString() +
+                            "\nRarity: " + Jsonresponse.achievements[i].rarity.currentCategory.ToString() +
+                            "\nPlayer Percentage: " + Jsonresponse.achievements[i].rarity.currentPercentage.ToString() +
+                            "%" +
+                            "\nSecret: " + Jsonresponse.achievements[i].isSecret.ToString() +
+                            "\nProgress State: " + Jsonresponse.achievements[i].progressState.ToString() + "\n",
+                            Jsonresponse.achievements[i].id.ToString()
+                        );
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("this game has no achivements", "fucky wucky");
+                Close();
             }
 
 
-            for (int i = 0; i < Jsonresponse.achievements.Count; i++)
-            {
-                //hacky thing because im kinda retarded and dont want to do proper ui design
-                Panel PanelLine = new Panel();
-                PanelLine.Height = 144;
-                PanelLine.Width = 782;
-                PanelLine.BackColor = Color.Transparent;
-                PanelLine.BorderStyle = BorderStyle.FixedSingle;
-                PanelLine.Location = new System.Drawing.Point(0, 23 + newline);
-                PanelLine.Name = "Line_" + i.ToString();
-                ALForm.Panel_AchievementList.Controls.Add(PanelLine);
-                var PanelLineElement = ALForm.Panel_AchievementList.Controls.OfType<Panel>().Last();
-                //background for unlocked section
-                Panel AchievementUnlockedBack = new Panel();
-                AchievementUnlockedBack.Enabled = false;
-                AchievementUnlockedBack.BorderStyle = BorderStyle.None;
-                AchievementUnlockedBack.BackColor = backcolour;
-                AchievementUnlockedBack.Location = new System.Drawing.Point(0, 0);
-                AchievementUnlockedBack.Size = new System.Drawing.Size(45, 144);
-                AchievementUnlockedBack.Name = "UnlockedBack_" + Jsonresponse.achievements[i].id.ToString();
-                PanelLineElement.Controls.Add(AchievementUnlockedBack);
-                //foreground for unlock section
-                CheckBox AchievementUnlocked = new CheckBox();
-                AchievementUnlocked.Location = new System.Drawing.Point(15, 64);
-                AchievementUnlocked.Text = " ";
-                AchievementUnlocked.Name = Jsonresponse.achievements[i].id.ToString();
-                AchievementUnlocked.Width = 15;
-                AchievementUnlocked.Height = 14;
-                AchievementUnlocked.BackColor = backcolour;
-                AchievementUnlocked.Click += new System.EventHandler(ALForm.SelectAchievement);
-                PanelLineElement.Controls.Add(AchievementUnlocked);
-                AchievementUnlocked.BringToFront();
-                //name section
-                Label AchievementName = new Label();
-                AchievementName.BackColor = backcolour;
-                AchievementName.Location = new System.Drawing.Point(47, 0);
-                AchievementName.MinimumSize = new System.Drawing.Size(149, 144);
-                AchievementName.MaximumSize = new System.Drawing.Size(149, 144);
-                AchievementName.Name = "Name_" + Jsonresponse.achievements[i].id.ToString();
-                AchievementName.Text = Jsonresponse.achievements[i].name.ToString();
-                PanelLineElement.Controls.Add(AchievementName);
-                AchievementName.BringToFront();
-                //description section
-                Label AchievementDescription = new Label();
-                AchievementDescription.BackColor = backcolour;
-                AchievementDescription.Location = new System.Drawing.Point(198, 0);
-                AchievementDescription.MinimumSize = new System.Drawing.Size(291, 144);
-                AchievementDescription.MaximumSize = new System.Drawing.Size(291, 144);
-                AchievementDescription.Name = "Description_" + Jsonresponse.achievements[i].id.ToString();
-                AchievementDescription.Text = Jsonresponse.achievements[i].description.ToString();
-                PanelLineElement.Controls.Add(AchievementDescription);
-                AchievementDescription.BringToFront();
-                //stats section
-                Label Stats = new Label();
-                Stats.BackColor = backcolour;
-                Stats.BorderStyle = BorderStyle.None;
-                Stats.Location = new System.Drawing.Point(491, 0);
-                Stats.MinimumSize = new System.Drawing.Size(292, 144);
-                Stats.MaximumSize = new System.Drawing.Size(292, 144);
-                Stats.Name = "Stats_" + Jsonresponse.achievements[i].id.ToString();
-                Stats.Text = "Gamerscore: " + Jsonresponse.achievements[i].rewards[0].value.ToString() +
-                             "\nRarity: " + Jsonresponse.achievements[i].rarity.currentCategory.ToString() +
-                             "\nPlayer Percentage: " + Jsonresponse.achievements[i].rarity.currentPercentage.ToString() + "%" +
-                             "\nSecret: " + Jsonresponse.achievements[i].isSecret.ToString() +
-                             "\nProgress State: " + Jsonresponse.achievements[i].progressState.ToString();
-                PanelLineElement.Controls.Add(Stats);
-                Stats.BringToFront();
-                /*foreground for images section
-                PictureBox AchievementImage = new PictureBox();
-                AchievementImage.BackColor = backcolour;
-                AchievementImage.BorderStyle = BorderStyle.None;
-                AchievementImage.Location = new System.Drawing.Point(491, 0);
-                AchievementImage.Size = new System.Drawing.Size(292, 144);
-                AchievementImage.Name = "Image_" + Jsonresponse.achievements[i].id.ToString();
-                AchievementImage.LoadAsync(Jsonresponse.achievements[i].mediaAssets[0].url.ToString()+ "&w=280&h=280&format=jpg");
-                AchievementImage.Visible = false;*/
-                //change some ui elements if achievement is already unlocked
-                if (Jsonresponse.achievements[i].progressState.ToString() == "Achieved")
-                {
-                    Stats.Text = "Gamerscore: " + Jsonresponse.achievements[i].rewards[0].value.ToString() +
-                                 "\nRarity: " + Jsonresponse.achievements[i].rarity.currentCategory.ToString() +
-                                 "\nPlayer Percentage: " + Jsonresponse.achievements[i].rarity.currentPercentage.ToString() + "%" +
-                                 "\nSecret: " + Jsonresponse.achievements[i].isSecret.ToString() +
-                                 "\nProgress State: " + Jsonresponse.achievements[i].progressState.ToString() +
-                                 "\nUnlock Time: " + Jsonresponse.achievements[i].progression.timeUnlocked.ToString();
-                    AchievementUnlocked.Enabled = false;
-                    AchievementUnlocked.Checked = true;
-                }
-                newline = newline + 144;
-                backcolour = Color.Silver;
-                if (i % 2 == 0)
-                {
-                    backcolour = Color.FromArgb(224, 224, 224);
-                }
-            }
+
         }
         void SelectAchievement(object sender, EventArgs e)
         {
@@ -159,27 +101,7 @@ namespace Xbox_Achievement_Unlocker
         }
         private void CheckBox_Images_CheckedChanged(object sender, EventArgs e)
         {
-            //man idk this doesnt work and I dont really care that much about images. leaving it for now
-            AchievementList ALForm = new AchievementList();
-            if (CheckBox_Images.Checked)
-            {
-                ALForm.Header4.Text = "Images";
-                foreach (PictureBox box in ALForm.Panel_AchievementList.Controls.OfType<PictureBox>())
-                {
-                    box.Visible = true;
-                    box.BringToFront();
-                }
-            }
-            else
-            {
-                ALForm.Header4.Text = "Stats";
-                foreach (PictureBox box in ALForm.Panel_AchievementList.Controls.OfType<PictureBox>())
-                {
-                    box.Visible = false;
-                    box.SendToBack();
-                }
-            }
-
+            //dont even know if I can put images in a data grid view lmao
         }
 
         void BTN_Unlock_Click(object sender, EventArgs e)
@@ -249,7 +171,6 @@ namespace Xbox_Achievement_Unlocker
             client.DefaultRequestHeaders.Add("Connection", "Keep-Alive");
             client.DefaultRequestHeaders.Add("User-Agent", "XboxServicesAPI/2021.04.20210610.3 c");
 
-            //foreach (string id in UnlockableAchievements)
             for (int i = 0; i < UnlockableAchievements.Count; i++)
             {
                 requestbody = requestbody + "{\"id\":\"" + UnlockableAchievements[i] + "\",\"percentComplete\":\"100\"},";
@@ -273,6 +194,21 @@ namespace Xbox_Achievement_Unlocker
 
                     requestbody = "{\"action\":\"progressUpdate\",\"serviceConfigId\":\"" + SCID + "\",\"titleId\":\"" + TitleID + "\",\"userId\":\"" + MainWindow.xuid + "\",\"achievements\":[";
                 }
+            }
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if ((int)DGV_AchievementList.Rows[e.RowIndex].Cells[0].Value == 0)
+            {
+                AchievementIDs.Add(DGV_AchievementList.Rows[e.RowIndex].Cells[4].Value.ToString());
+                DGV_AchievementList.Rows[e.RowIndex].Cells[0].Value = 1;
+            }
+            else if ((int)DGV_AchievementList.Rows[e.RowIndex].Cells[0].Value == 1)
+            {
+                DGV_AchievementList.Rows[e.RowIndex].Cells[0].Value = 0;
+                AchievementIDs.Remove(DGV_AchievementList.Rows[e.RowIndex].Cells[4].Value.ToString());
             }
 
         }
