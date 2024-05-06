dc40db3adaf6833c769fdaf383e89195593b636c
szaaamerik <126014478+szaaamerik@users.noreply.github.com>
2024-05-06 09:59:30 -0700

    fix(app): add theme constants to the app.xaml file and change some stuff in app.xaml.cs as well

--- XAU/App.xaml.cs
+++ XAU/App.xaml.cs
@@ -1,115 +1,83 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.IO;
-using System.Reflection;
-using System.Windows.Threading;
-using Wpf.Ui.Controls;
 using XAU.Services;
 using XAU.ViewModels.Pages;
 using XAU.ViewModels.Windows;
 using XAU.Views.Pages;
 using XAU.Views.Windows;
 
+namespace XAU;
 
-namespace XAU
+public partial class App 
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App 
+    private static readonly IHost Host = Microsoft.Extensions.Hosting.Host
+        .CreateDefaultBuilder()
+        .ConfigureServices((_, services) =>
+        {
+            services.AddHostedService<ApplicationHostService>();
+
+            services.AddSingleton<MainWindow>();
+            services.AddSingleton<MainWindowViewModel>();
+            services.AddSingleton<INavigationService, NavigationService>();
+            services.AddSingleton<ISnackbarService, SnackbarService>();
+            services.AddSingleton<IContentDialogService, ContentDialogService>();
+
+            services.AddSingleton<HomePage>();
+            services.AddSingleton<HomeViewModel>();
+            services.AddSingleton<SettingsPage>();
+            services.AddSingleton<SettingsViewModel>();
+            services.AddSingleton<GamesPage>();
+            services.AddSingleton<GamesViewModel>();
+            services.AddSingleton<AchievementsPage>();
+            services.AddSingleton<AchievementsViewModel>();
+            services.AddSingleton<PlaceholderPage>();
+            services.AddSingleton<StatsPage>();
+            services.AddSingleton<StatsViewModel>();
+            services.AddSingleton<MiscPage>();
+            services.AddSingleton<MiscViewModel>();
+            services.AddSingleton<InfoPage>();
+            services.AddSingleton<InfoViewModel>();
+        }).Build();
+
+    private static T? GetService<T>() where T : class
     {
-        // The.NET Generic Host provides dependency injection, configuration, logging, and other services.
-        // https://docs.microsoft.com/dotnet/core/extensions/generic-host
-        // https://docs.microsoft.com/dotnet/core/extensions/dependency-injection
-        // https://docs.microsoft.com/dotnet/core/extensions/configuration
-        // https://docs.microsoft.com/dotnet/core/extensions/logging
-        private static readonly IHost _host = Host
-            .CreateDefaultBuilder()
-            .ConfigureAppConfiguration(c => { c.SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)); })
-            .ConfigureServices((context, services) =>
-            {
-                services.AddHostedService<ApplicationHostService>();
+        return Host.Services.GetService(typeof(T)) as T;
+    }
 
-                services.AddSingleton<MainWindow>();
-                services.AddSingleton<MainWindowViewModel>();
-                services.AddSingleton<INavigationService, NavigationService>();
-                services.AddSingleton<ISnackbarService, SnackbarService>();
-                services.AddSingleton<IContentDialogService, ContentDialogService>();
+    private void OnStartup(object sender, StartupEventArgs e)
+    {
+        Host.Start();
+        SetupExceptionHandling();
+    }
 
-                services.AddSingleton<HomePage>();
-                services.AddSingleton<HomeViewModel>();
-                services.AddSingleton<SettingsPage>();
-                services.AddSingleton<SettingsViewModel>();
-                services.AddSingleton<GamesPage>();
-                services.AddSingleton<GamesViewModel>();
-                services.AddSingleton<AchievementsPage>();
-                services.AddSingleton<AchievementsViewModel>();
-                services.AddSingleton<PlaceholderPage>();
-                services.AddSingleton<StatsPage>();
-                services.AddSingleton<StatsViewModel>();
-                services.AddSingleton<MiscPage>();
-                services.AddSingleton<MiscViewModel>();
-                services.AddSingleton<InfoPage>();
-                services.AddSingleton<InfoViewModel>();
+    private async void OnExit(object sender, ExitEventArgs e)
+    {
+        await Host.StopAsync();
+        Host.Dispose();
+    }
 
-            }).Build();
-
-        /// <summary>
-        /// Gets registered service.
-        /// </summary>
-        /// <typeparam name="T">Type of the service to get.</typeparam>
-        /// <returns>Instance of the service or <see langword="null"/>.</returns>
-        public static T GetService<T>()
-            where T : class
+    private void SetupExceptionHandling()
+    {
+        AppDomain.CurrentDomain.UnhandledException += (_, e) =>
         {
-            return _host.Services.GetService(typeof(T)) as T;
-        }
-
-        /// <summary>
-        /// Occurs when the application is loading.
-        /// </summary>
-        private void OnStartup(object sender, StartupEventArgs e)
+            ReportException((Exception)e.ExceptionObject);
+        };
+        
+        DispatcherUnhandledException += (_, e) =>
         {
-            _host.Start();
-            SetupExceptionHandling();
-        }
-
-        /// <summary>
-        /// Occurs when the application is closing.
-        /// </summary>
-        private async void OnExit(object sender, ExitEventArgs e)
+            ReportException(e.Exception);
+            e.Handled = true;
+        };
+        
+        TaskScheduler.UnobservedTaskException += (_, e) =>
         {
-            await _host.StopAsync();
-
-            _host.Dispose();
-        }
-
-        /// <summary>
-        /// Occurs when an exception is thrown by an application but not handled.
-        /// </summary>
-        private void SetupExceptionHandling()
-        {
-            AppDomain.CurrentDomain.UnhandledException += (_, e) =>
-                ReportException((Exception)e.ExceptionObject, "AppDomain.CurrentDomain.UnhandledException");
-            DispatcherUnhandledException += (_, e) =>
-            {
-                ReportException(e.Exception, "Application.Current.DispatcherUnhandledException");
-                e.Handled = true;
-            };
-            TaskScheduler.UnobservedTaskException += (_, e) =>
-            {
-                ReportException(e.Exception, "TaskScheduler.UnobservedTaskException");
-                e.SetObserved();
-            };
-        }
-        private async void ReportException(Exception exception, string source)
-        {
-            var mainWindowViewModel = GetService<MainWindowViewModel>();
-            if (mainWindowViewModel != null)
-            {
-                mainWindowViewModel.ShowErrorDialog(exception);
-            }
-        }
+            ReportException(e.Exception);
+            e.SetObserved();
+        };
+    }
+    private static void ReportException(Exception exception)
+    {
+        var mainWindowViewModel = GetService<MainWindowViewModel>();
+        mainWindowViewModel?.ShowErrorDialog(exception);
     }
 }
