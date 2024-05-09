f2c55eff6531fd91147b35b238c0dbfe92513e39
Draff <comamnds@outlook.com>
2024-05-09 01:18:54 +0100

    Add Debug Page with event replacement tester

---
+++ XAU/Views/Pages/DebugPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using Wpf.Ui.Controls;
+using XAU.ViewModels.Pages;
+
+namespace XAU.Views.Pages
+{
+    public partial class DebugPage : INavigableView<DebugViewModel>
+    {
+        public DebugViewModel ViewModel { get; }
+
+        public DebugPage(DebugViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+            InitializeComponent();
+        }
+    }
+}
