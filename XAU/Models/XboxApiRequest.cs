8f571eb5a441ff77b0ceb0ea32d892fae0472169
andrew <andrew@forming.ai>
2024-05-16 11:43:09 -0600

    dotnet format

--- XAU/Models/XboxApiRequest.cs
+++ XAU/Models/XboxApiRequest.cs
@@ -1,12 +1,12 @@
 public class HeartbeatRequest
 {
-public List<TitleRequest> titles {get; set;}
+    public List<TitleRequest> titles { get; set; }
 }
 
 public class TitleRequest
 {
-    public int expiration {get; set;}
-    public string id {get; set;}
-    public string state {get; set;}
-    public string sandbox {get; set;}
+    public int expiration { get; set; }
+    public string id { get; set; }
+    public string state { get; set; }
+    public string sandbox { get; set; }
 }
