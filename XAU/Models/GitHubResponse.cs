a50a06e989ff95a051b0eb33fcad202b90251d8b
🅰️🅰️🅰️1️⃣6️⃣2️⃣ ✔️ <admin@aaa162.live>
2025-09-07 19:48:03 -0500

    Fix title id search

--- XAU/Models/GitHubResponse.cs
+++ XAU/Models/GitHubResponse.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 public class EventsUpdateResponse
 {
     public int Timestamp { get; set; }
@@ -8,4 +10,23 @@ public class VersionResponse
 {
     public string? DownloadURL { get; set; }
     public string? LatestBuildVersion { get; set; }
+}
+
+
+public class GitHubFile
+{
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("path")]
+    public string Path { get; set; }
+
+    [JsonProperty("sha")]
+    public string Sha { get; set; }
+
+    [JsonProperty("size")]
+    public long Size { get; set; }
+
+    [JsonProperty("download_url")]
+    public string DownloadUrl { get; set; }
 }
---
+++ XAU/Models/MiscItems.cs
@@ -0,0 +1,7 @@
+
+    public class GameItem
+    {
+        public string Title { get; set; }
+        public string TitleId { get; set; }
+        public bool IsTitleBased { get; set; }
+    }
